e9b421eda89601693714683de30124ba7d18a581
rikukousaka7 <kuroyukiriku@gmail.com>
2020-02-24 02:43:59 +0700

    Halodoc Script Automation
    //Create Provisioning TestCase Activity

--- Object Repository/Provisioning/titletxt_InsertNumber.rs
+++ Object Repository/Provisioning/titletxt_InsertNumber.rs
@@ -4,6 +4,12 @@
    <name>titletxt_InsertNumber</name>
    <tag></tag>
    <elementGuidId>00000000-0000-0000-0000-000000000000</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>//*[@class = 'android.widget.TextView' and @resource-id = 'com.linkdokter.halodoc.android:id/tvEnterMobileNumber']</value>
+      </entry>
+   </selectorCollection>
    <selectorMethod>BASIC</selectorMethod>
    <useRalativeImagePath>false</useRalativeImagePath>
    <webElementProperties>
@@ -14,7 +20,7 @@
       <value>android.widget.TextView</value>
    </webElementProperties>
    <webElementProperties>
-      <isSelected>true</isSelected>
+      <isSelected>false</isSelected>
       <matchCondition>equals</matchCondition>
       <name>text</name>
       <type>Main</type>
--- Scripts/TestCase Activity/Provisioning/Script1582477650802.groovy
+++ Scripts/TestCase Activity/Provisioning/Script1582477650802.groovy
@@ -15,3 +15,24 @@ import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
 
+Mobile.startExistingApplication('C:\\Users\\admin\\Documents\\Prepare for New Job\\halodoc\\Halodoc Doctors Medicine Appointments_v6.600_apkpure.com.apk', 
+    FailureHandling.STOP_ON_FAILURE)
+
+Mobile.waitForElementPresent(findTestObject('Provisioning/txtview_NomorPonsel'), 0)
+
+Mobile.tap(findTestObject('Provisioning/edittxt_NomorPonsel'), 0)
+
+Mobile.waitForElementPresent(findTestObject('Provisioning/titletxt_InsertNumber'), 0)
+
+Mobile.setText(findTestObject('Provisioning/edittxt_PhoneNumVerif'), '081239301331', 0)
+
+Mobile.hideKeyboard()
+
+Mobile.tap(findTestObject('Provisioning/btn_TermNCond'), 0)
+
+Mobile.waitForElementPresent(findTestObject('Provisioning/titleview_TermNCond'), 0)
+
+Mobile.tap(findTestObject('Provisioning/btn_BackTermView'), 0)
+
+Mobile.tap(findTestObject('Provisioning/btn_Verif'), 0)
+
