307d06a092682d9583305008b5a24e1d061a0ba5
rikukousaka7 <kuroyukiriku@gmail.com>
2020-02-24 00:09:53 +0700

    Halodoc Script Automation
    Add new Object on Obj Repository
    Create Hierarcy Test Set

---
+++ Test Suites/TSA_Halodoc_001.ts
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TSA_Halodoc_001</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>eb29815a-f703-4833-807e-d5c0102e7554</testSuiteGuid>
+</TestSuiteEntity>
